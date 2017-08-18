1a553514feaead038f83ab56cb28612d660019ef
LAPTOP-J49RA32S\rodri <rodri.alvarellos@gmail.com>
2017-08-17 22:12:56 -0300

    Correccion carga de entidades

--- src/main/java/com/inclusion/Application.java
+++ src/main/java/com/inclusion/Application.java
@@ -23,203 +23,157 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
-		
 	}
 	
 	@Bean
 	public boolean loadEntidades(){
 		
-		Entidad ent1 = new Entidad();
-		ent1.setNombre("Fundacion F.A.R.O.S.");
-		ent1.setDireccion("Haedo");
-		ent1.setNumeroDireccion(473);
-		ent1.setLatitud(-31.4135551);
-		ent1.setLongitud(-64.21298130000002);
-		ent1.setTelefono("0351 - 4809735" );
-		ent1.setWeb("http://www.fundacionfaros.org.ar/contact");
-		entidadRepository.save(ent1);
+		Entidad ent = new Entidad();
 
+		ent.setNombre("Fundacion F.A.R.O.S.");
+		ent.setDireccion("Haedo");
+		ent.setNumeroDireccion(473);
+		ent.setLatitud(-31.4135551);
+		ent.setLongitud(-64.21298130000002);
+		ent.setTelefono("0351 - 4809735" );
+		ent.setWeb("http://www.fundacionfaros.org.ar/contact");
+		entidadRepository.save(ent);
+		//---------------------------------------------------		
+		ent.setNombre("CILSA");
+		ent.setDireccion("Coronel Agustin Olmedo");
+		ent.setNumeroDireccion(180);
+		ent.setLatitud(-31.4091605);
+		ent.setLongitud(-64.19939369999997);
+		ent.setTelefono("0351 425-8900");
+		ent.setWeb("https://www.cilsa.org/contacto/");
+		entidadRepository.save(ent);		
 		//---------------------------------------------------
-
-		Entidad ent2 = new Entidad();
-		ent2.setNombre("CILSA");
-		ent2.setDireccion("Coronel Agustin Olmedo");
-		ent2.setNumeroDireccion(180);
-		ent2.setLatitud(-31.4091605);
-		ent2.setLongitud(-64.19939369999997);
-		ent2.setTelefono("0351 425-8900");
-		ent2.setWeb("https://www.cilsa.org/contacto/");
-		entidadRepository.save(ent2);
-		
+		ent.setNombre("Fundacion La Estacion");
+		ent.setDireccion("Adolfo Orma");
+		ent.setNumeroDireccion(1446);
+		ent.setLatitud(-31.3830075);
+		ent.setLongitud(-64.2300262);
+		ent.setTelefono("54-351-4823764");
+		ent.setEmail("fundacionlaestacion@hotmail.com");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent3 = new Entidad();
-		ent3.setNombre("Fundacion La Estacion");
-		ent3.setDireccion("Adolfo Orma");
-		ent3.setNumeroDireccion(1446);
-		ent3.setLatitud(-31.3830075);
-		ent3.setLongitud(-64.2300262);
-		ent3.setTelefono("54-351-4823764");
-		ent3.setEmail("fundacionlaestacion@hotmail.com");
-		entidadRepository.save(ent3);
-
+		ent.setNombre("Instituto de Rehabilitacion Cordoba");
+		ent.setDireccion("San Lorenzo");
+		ent.setNumeroDireccion(283);
+		ent.setLatitud(-31.4241422 );
+		ent.setLongitud(-64.18379119999997);
+		ent.setTelefono("0351 422 8183");
+		ent.setEmail("alpicordoba@ciudad.com.ar");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent4 = new Entidad();
-		ent4.setNombre("Instituto de Rehabilitacion Cordoba");
-		ent4.setDireccion("San Lorenzo");
-		ent4.setNumeroDireccion(283);
-		ent4.setLatitud(-31.4241422 );
-		ent4.setLongitud(-64.18379119999997);
-		ent4.setTelefono("0351 422 8183");
-		ent4.setEmail("alpicordoba@ciudad.com.ar");
-		entidadRepository.save(ent4);
-
+		ent.setNombre("Fundacion Pueblo Nuevo");
+		ent.setDireccion("General Alvear");
+		ent.setNumeroDireccion(351);
+		ent.setLatitud(-31.4127161);
+		ent.setLongitud(-64.17970600000001);
+		ent.setTelefono("0351 424-0599");
+		ent.setWeb("http://fundacionpueblonuevo.blogspot.com.ar/");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent5 = new Entidad();
-		ent5.setNombre("Fundacion Pueblo Nuevo");
-		ent5.setDireccion("General Alvear");
-		ent5.setNumeroDireccion(351);
-		ent5.setLatitud(-31.4127161);
-		ent5.setLongitud(-64.17970600000001);
-		ent5.setTelefono("0351 424-0599");
-		ent5.setWeb("http://fundacionpueblonuevo.blogspot.com.ar/");
-		entidadRepository.save(ent5);
-
+		ent.setNombre("Fundacion La Mirada");
+		ent.setDireccion("Pedro de Mendoza");
+		ent.setNumeroDireccion(2634);
+		ent.setLatitud(-31.4410872);
+		ent.setLongitud(-64.20698449999998);
+		ent.setTelefono("0351 652-6726");
+		ent.setEmail("fundacionlamirada@gmail.com");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent6 = new Entidad();
-		ent6.setNombre("Fundacion La Mirada");
-		ent6.setDireccion("Pedro de Mendoza");
-		ent6.setNumeroDireccion(2634);
-		ent6.setLatitud(-31.4410872);
-		ent6.setLongitud(-64.20698449999998);
-		ent6.setTelefono("0351 652-6726");
-		ent6.setEmail("fundacionlamirada@gmail.com");
-		entidadRepository.save(ent6);
-
+		ent.setNombre("Crescomas");
+		ent.setDireccion("Santiago del Estero");
+		ent.setNumeroDireccion(273);
+		ent.setLatitud(-31.41488);
+		ent.setLongitud(-64.17620399999998);
+		ent.setTelefono("0351 6240098") ;
+		ent.setEmail("crescomascba@gmail.com");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent7 = new Entidad();
-		ent7.setNombre("Crescomas");
-		ent7.setDireccion("Santiago del Estero");
-		ent7.setNumeroDireccion(273);
-		ent7.setLatitud(-31.41488);
-		ent7.setLongitud(-64.17620399999998);
-		ent7.setTelefono("0351 6240098") ;
-		ent7.setEmail("crescomascba@gmail.com");
-		entidadRepository.save(ent7);
-
+		ent.setNombre("Por igual mas");
+		ent.setDireccion("Juan del Campillo");
+		ent.setNumeroDireccion(173);
+		ent.setLatitud(-31.4015043);
+		ent.setLongitud(-64.17958599999997);
+		ent.setTelefono("0351 152486437");
+		ent.setEmail("porigual.comunicaciones@gmail.com");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent8 = new Entidad();
-		ent8.setNombre("Por igual mas");
-		ent8.setDireccion("Juan del Campillo");
-		ent8.setNumeroDireccion(173);
-		ent8.setLatitud(-31.4015043);
-		ent8.setLongitud(-64.17958599999997);
-		ent8.setTelefono("0351 152486437");
-		ent8.setEmail("porigual.comunicaciones@gmail.com");
-		entidadRepository.save(ent8);
-
+		ent.setNombre("Fundacion Senda Azul");
+		ent.setDireccion("Dumesnil");
+		ent.setNumeroDireccion(1440);
+		ent.setLatitud(-31.4012455);
+		ent.setLongitud(-64.19812580000001);
+		ent.setTelefono("0351 4731297");
+		ent.setEmail("fundacionsendaazul@live.com.ar");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent9 = new Entidad();
-		ent9.setNombre("Fundacion Senda Azul");
-		ent9.setDireccion("Dumesnil");
-		ent9.setNumeroDireccion(1440);
-		ent9.setLatitud(-31.4012455);
-		ent9.setLongitud(-64.19812580000001);
-		ent9.setTelefono("0351 4731297");
-		ent9.setEmail("fundacionsendaazul@live.com.ar");
-		entidadRepository.save(ent9);
-
+		ent.setNombre("Inclubyte");
+		ent.setDireccion("Javier Lascano Colodrero");
+		ent.setNumeroDireccion(2962);
+		ent.setLatitud(-31.3630431);
+		ent.setLongitud(-64.211728) ;
+		ent.setTelefono("0351 477-0245");
+		ent.setEmail("fundacioninclubyte@gmail.com");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent10 = new Entidad();
-		ent10.setNombre("Inclubyte");
-		ent10.setDireccion("Javier Lascano Colodrero");
-		ent10.setNumeroDireccion(2962);
-		ent10.setLatitud(-31.3630431);
-		ent10.setLongitud(-64.211728) ;
-		ent10.setTelefono("0351 477-0245");
-		ent10.setEmail("fundacioninclubyte@gmail.com");
-		entidadRepository.save(ent10);
-
+		ent.setNombre("Usuarios, Familiares y Amigos de los Servicios de Salud Mental");
+		ent.setDireccion("Leon Morra");
+		ent.setNumeroDireccion(172);
+		ent.setLatitud(-31.4190659);
+		ent.setLongitud(-64.17061919999998);
+		ent.setTelefono("3514951969");
+		ent.setEmail("saludmentalcordoba@gmail.com");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent11 = new Entidad();
-		ent11.setNombre("Usuarios, Familiares y Amigos de los Servicios de Salud Mental");
-		ent11.setDireccion("Leon Morra");
-		ent11.setNumeroDireccion(172);
-		ent11.setLatitud(-31.4190659);
-		ent11.setLongitud(-64.17061919999998);
-		ent11.setTelefono("3514951969");
-		ent11.setEmail("saludmentalcordoba@gmail.com");
-		entidadRepository.save(ent11);
-
+		ent.setNombre("Pequenio Cottolengo Don Orione");
+		ent.setDireccion("Av. Armada Argentina");
+		ent.setNumeroDireccion(2440);
+		ent.setLatitud(-31.47791819999999);
+		ent.setLongitud(-64.2377368) ;
+		ent.setTelefono("0800 444 3274");
+		ent.setWeb("http://www.donorionecordoba.com.ar/");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent12 = new Entidad();
-		ent12.setNombre("Pequenio Cottolengo Don Orione");
-		ent12.setDireccion("Av. Armada Argentina");
-		ent12.setNumeroDireccion(2440);
-		ent12.setLatitud(-31.47791819999999);
-		ent12.setLongitud(-64.2377368) ;
-		ent12.setTelefono("0800 444 3274");
-		ent12.setWeb("http://www.donorionecordoba.com.ar/");
-		entidadRepository.save(ent12);
-
+		ent.setNombre("Fundacion Si Cordoba");
+		ent.setDireccion("Dean Funes");
+		ent.setNumeroDireccion(100);
+		ent.setLatitud(-31.4151981);
+		ent.setLongitud(-64.18663129999999) ;
+		ent.setTelefono("0351 155426938");
+		ent.setEmail("cordoba@fundacionsi.org.ar");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent13 = new Entidad();
-		ent13.setNombre("Fundacion Si Cordoba");
-		ent13.setDireccion("Dean Funes");
-		ent13.setNumeroDireccion(100);
-		ent13.setLatitud(-31.4151981);
-		ent13.setLongitud(-64.18663129999999) ;
-		ent13.setTelefono("0351 155426938");
-		ent13.setEmail("cordoba@fundacionsi.org.ar");
-		entidadRepository.save(ent13);
-
+		ent.setNombre("Fundacion Baccigalupo Deporte");
+		ent.setDireccion("Agustin Garzon");
+		ent.setNumeroDireccion(1221);
+		ent.setLatitud(-31.424027);
+		ent.setLongitud(-64.16720609999999);
+		ent.setTelefono("0351 155500347");
+		ent.setWeb("http://fundacionbaccigalupo.org/");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent14 = new Entidad();
-		ent14.setNombre("Fundacion Baccigalupo Deporte");
-		ent14.setDireccion("Agustin Garzon");
-		ent14.setNumeroDireccion(1221);
-		ent14.setLatitud(-31.424027);
-		ent14.setLongitud(-64.16720609999999);
-		ent14.setTelefono("0351 155500347");
-		ent14.setWeb("http://fundacionbaccigalupo.org/");
-		entidadRepository.save(ent14);
-
+		ent.setNombre("Powerchair Football Argentina");
+		ent.setDireccion("Av. Carcano sn");
+		ent.setNumeroDireccion(0);
+		ent.setLatitud(-31.368864);
+		ent.setLongitud(-64.246113) ;
+		ent.setTelefono("011 47082620");
+		ent.setEmail("adriana.borgogno@pcfa.com.ar");
+		entidadRepository.save(ent);
 		//---------------------------------------------------
-
-		Entidad ent15 = new Entidad();
-		ent15.setNombre("Powerchair Football Argentina");
-		ent15.setDireccion("Av. Carcano sn");
-		ent15.setNumeroDireccion(0);
-		ent15.setLatitud(-31.368864);
-		ent15.setLongitud(-64.246113) ;
-		ent15.setTelefono("011 47082620");
-		ent15.setEmail("adriana.borgogno@pcfa.com.ar");
-		entidadRepository.save(ent15);
-
-		//---------------------------------------------------
-
-		Entidad ent16 = new Entidad();
-		ent16.setNombre("Apadro");
-		ent16.setDireccion("Elpidio Gonzalez");
-		ent16.setNumeroDireccion(302);
-		ent16.setLatitud(-31.2931162);
-		ent16.setLongitud(-64.30208590000001) ;
-		ent16.setTelefono("0351 153506909");
-		ent16.setEmail("apadro_va@hotmail.com");
-		entidadRepository.save(ent16);
+		ent.setNombre("Apadro");
+		ent.setDireccion("Elpidio Gonzalez");
+		ent.setNumeroDireccion(302);
+		ent.setLatitud(-31.2931162);
+		ent.setLongitud(-64.30208590000001);
+		ent.setTelefono("0351 153506909");
+		ent.setEmail("apadro_va@hotmail.com");
+		entidadRepository.save(ent);
 
 		return true;
 	}
-	
 }
